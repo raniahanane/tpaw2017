24639fc60fab3913732999bac7ab52b1a42fe963
raniahanane <layounerania@gmail.com>
2016-10-21 14:03:52 +0200

    Update rania.js

--- tp3/rania.js
+++ tp3/rania.js
@@ -1,5 +1,20 @@
 $(function(){
-   if($("#nom").val()=''){
+   if($("#Name").val()=""){
+      $(.'modal').modal('show');
+      }
+   
+      if($("#Prenom").val()=""){
+      $(.'modal').modal('show');
+      }
+   
+     if($("#Date de naissance").val()=""){
+      $(.'modal').modal('show');
+      }
+   
+        if($("#Email").val()=""){
+      $(.'modal').modal('show');
+      }
+           if($("#Adresse").val()=""){
       $(.'modal').modal('show');
       }
 }); 
