a86d94d0d27b558a2480067c9e4d42ad035c8682
raniahanane <layounerania@gmail.com>
2016-10-21 16:40:43 +0200

    Add files via upload

--- tp3/rania.js
+++ tp3/rania.js
@@ -1,20 +1,38 @@
-$(document).ready(function){
+$(document).ready(function(){
 $("#bouton").click(function (){                   
-      if($("#Name").val()=""){
-      $("Mymodel").modal();
+      if($("#Name").val()==""){
+      $("#Mymodel").modal();
       }   
-      if($("#Prenom").val()=""){
-     $("Mymodel").modal();
+      if($("#Prenom").val()==""){
+     $("#Mymodel").modal();
       }
    
-     if($("#Date de naissance").val()=""){
-     $("Mymodel").modal();
+     if($("#Date de naissance").val()==""){
+     $("#Mymodel").modal();
       }
    
-        if($("#Email").val()=""){
-      $("Mymodel").modal();
+        if($("#Email").val()==''){
+      $("#Mymodel").modal();
       }
-           if($("#Adresse").val()=""){
-      $("Mymodel").modal();
-      }           
+           if($("#Adresse").val()==""){
+      $("#Mymodel").modal(); 
+      }  
+      if($("#Name").val()=="rania"){
+      $("#Mymodel2").modal();
+      }   
+      if($("#Prenom").val()=="hanane"){
+     $("#Mymodel2").modal();
+      }
+   
+     if($("#Date de naissance").val()=="08/10/1993"){
+     $("#Mymodel2").modal();
+      }
+   
+        if($("#Email").val()=='layounerania@gmail.com'){
+      $("#Mymodel2").modal();
+      }
+           if($("#Adresse").val()=="Avenue des champs elysees Paris"){
+      $("#Mymodel2").modal(); 
+      }	  
    });
+ });
